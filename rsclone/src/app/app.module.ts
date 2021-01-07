c3e5a05470d82df164a4c7f04e20d200062a9984
Maksim Burakou <maxashtet@gmail.com>
2021-01-07 11:01:46 +0300

    feat: add content component

--- rsclone/src/app/app.module.ts
+++ rsclone/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
