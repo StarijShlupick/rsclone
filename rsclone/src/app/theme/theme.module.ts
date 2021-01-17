17967ecd65a99a991dece66148f3657084fc45fc
Maksim Burakou <maxashtet@gmail.com>
2021-01-17 20:42:07 +0300

    refact: delete extra empty strings

--- rsclone/src/app/theme/theme.module.ts
+++ rsclone/src/app/theme/theme.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from './theme.service';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
