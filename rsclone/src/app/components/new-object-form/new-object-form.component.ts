9b2c6a21160bcf54a641390f44f329819ecca789
Volha Palkhouskaya <vpalkhouskaya@scnsoft.com>
2021-01-21 16:15:08 +0300

    add: test map

--- rsclone/src/app/components/new-object-form/new-object-form.component.ts
+++ rsclone/src/app/components/new-object-form/new-object-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-new-object-form',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewObjectFormComponent implements OnInit {
 
-  constructor() { }
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 45.899977;
+  lng = 6.172652;
+  zoom = 12;
+
+  constructor() {
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+   }
 
   ngOnInit(): void {
+
+    this.buildMap();
+  }
+
+  buildMap(): void {
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: this.zoom,
+      center: [this.lng, this.lat]
+    });
+   this.map.addControl(new mapboxgl.NavigationControl());
   }
 
 }
