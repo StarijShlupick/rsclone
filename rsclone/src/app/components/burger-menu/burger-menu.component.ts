458b9ececcec72f75eeb7b7950d204f3883199bc
Maksim Burakou <maxashtet@gmail.com>
2021-01-25 23:21:42 +0300

    fix: delete ngOnInit and constructou, extra angular animations

--- rsclone/src/app/components/burger-menu/burger-menu.component.ts
+++ rsclone/src/app/components/burger-menu/burger-menu.component.ts
@@ -1,14 +1,29 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
-  styleUrls: ['./burger-menu.component.scss']
+  styleUrls: ['./burger-menu.component.scss'],
+  animations: [
+    trigger('menu', [
+      transition('void => *', [
+        style({
+          left: '-400px'
+        }),
+        animate('250ms ease-out')
+      ])
+    ]),
+    trigger('backdrop', [
+      transition('void => *', [
+        style({
+          opacity: '0'
+        }),
+        animate('250ms ease-out')
+      ])
+    ])
+  ]
 })
-export class BurgerMenuComponent implements OnInit {
+export class BurgerMenuComponent {
   @Output() close = new EventEmitter<void>();
-  constructor() { }
-  ngOnInit(): void {
-  }
-
 }
