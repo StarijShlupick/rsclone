dff42c617c4cc9f02582189f7d80d833366fe24d
Maksim Burakou <maxashtet@gmail.com>
2021-02-02 12:04:16 +0300

    feat: add translation for second-screen

--- rsclone/src/app/services/waste.service.ts
+++ rsclone/src/app/services/waste.service.ts
@@ -9,12 +9,26 @@ export class WasteService {
   allowPaper = [];
   notAllowPaper = [];
   titlePlastic = '';
+  allowPlastic = [];
+  notAllowPlastic = [];
   titleGlass = '';
+  allowGlass = [];
+  notAllowGlass = [];
   titleMetal = '';
+  allowMetal = [];
+  notAllowMetal = [];
   titleHazardous = '';
+  allowHazardous = [];
+  notAllowHazardous = [];
   titleLightBulb = '';
+  allowLightBulb = [];
+  notAllowLightBulb = [];
   titleThings = '';
+  allowThings = [];
+  notAllowThings = [];
   titleBattery = '';
+  allowBattery = [];
+  notAllowBattery = [];
   private waitChanges$: Subject<void> = new Subject();
   wasteItems: IWasteItem[];
 
@@ -24,23 +38,51 @@ export class WasteService {
         'SECOND_SCREEN.WASTE-ITEMS.PAPER.ALLOW',
         'SECOND_SCREEN.WASTE-ITEMS.PAPER.NOT-ALLOW',
         'SECOND_SCREEN.WASTE-ITEMS.PLASTIC.TITLE',
+        'SECOND_SCREEN.WASTE-ITEMS.PLASTIC.ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.PLASTIC.NOT-ALLOW',
         'SECOND_SCREEN.WASTE-ITEMS.GLASS.TITLE',
+        'SECOND_SCREEN.WASTE-ITEMS.GLASS.ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.GLASS.NOT-ALLOW',
         'SECOND_SCREEN.WASTE-ITEMS.METAL.TITLE',
+        'SECOND_SCREEN.WASTE-ITEMS.METAL.ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.METAL.NOT-ALLOW',
         'SECOND_SCREEN.WASTE-ITEMS.HAZARDOUS.TITLE',
+        'SECOND_SCREEN.WASTE-ITEMS.HAZARDOUS.ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.HAZARDOUS.NOT-ALLOW',
         'SECOND_SCREEN.WASTE-ITEMS.LIGHT-BULB.TITLE',
+        'SECOND_SCREEN.WASTE-ITEMS.LIGHT-BULB.ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.LIGHT-BULB.NOT-ALLOW',
         'SECOND_SCREEN.WASTE-ITEMS.THINGS.TITLE',
-        'SECOND_SCREEN.WASTE-ITEMS.BATTERY.TITLE'])
+        'SECOND_SCREEN.WASTE-ITEMS.THINGS.ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.THINGS.NOT-ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.BATTERY.TITLE',
+        'SECOND_SCREEN.WASTE-ITEMS.BATTERY.ALLOW',
+        'SECOND_SCREEN.WASTE-ITEMS.BATTERY.NOT-ALLOW'])
       .subscribe((translations) => {
         this.titlePaper = translations['SECOND_SCREEN.WASTE-ITEMS.PAPER.TITLE'];
         this.allowPaper = translations['SECOND_SCREEN.WASTE-ITEMS.PAPER.ALLOW'].split('; ');
         this.notAllowPaper = translations['SECOND_SCREEN.WASTE-ITEMS.PAPER.NOT-ALLOW'].split('; ');
         this.titlePlastic = translations['SECOND_SCREEN.WASTE-ITEMS.PLASTIC.TITLE'];
+        this.allowPlastic = translations['SECOND_SCREEN.WASTE-ITEMS.PLASTIC.ALLOW'].split('; ');
+        this.notAllowPlastic = translations['SECOND_SCREEN.WASTE-ITEMS.PLASTIC.NOT-ALLOW'].split('; ');
         this.titleGlass = translations['SECOND_SCREEN.WASTE-ITEMS.GLASS.TITLE'];
+        this.allowGlass = translations['SECOND_SCREEN.WASTE-ITEMS.GLASS.ALLOW'].split('; ');
+        this.notAllowGlass = translations['SECOND_SCREEN.WASTE-ITEMS.GLASS.NOT-ALLOW'].split('; ');
         this.titleMetal = translations['SECOND_SCREEN.WASTE-ITEMS.METAL.TITLE'];
+        this.allowMetal = translations['SECOND_SCREEN.WASTE-ITEMS.METAL.ALLOW'].split('; ');
+        this.notAllowMetal = translations['SECOND_SCREEN.WASTE-ITEMS.METAL.NOT-ALLOW'].split('; ');
         this.titleHazardous = translations['SECOND_SCREEN.WASTE-ITEMS.HAZARDOUS.TITLE'];
+        this.allowHazardous = translations['SECOND_SCREEN.WASTE-ITEMS.HAZARDOUS.ALLOW'].split('; ');
+        this.notAllowHazardous = translations['SECOND_SCREEN.WASTE-ITEMS.HAZARDOUS.NOT-ALLOW'].split('; ');
         this.titleLightBulb = translations['SECOND_SCREEN.WASTE-ITEMS.LIGHT-BULB.TITLE'];
+        this.allowLightBulb = translations['SECOND_SCREEN.WASTE-ITEMS.LIGHT-BULB.ALLOW'].split('; ');
+        this.notAllowLightBulb = translations['SECOND_SCREEN.WASTE-ITEMS.LIGHT-BULB.NOT-ALLOW'].split('; ');
         this.titleThings = translations['SECOND_SCREEN.WASTE-ITEMS.THINGS.TITLE'];
+        this.allowThings = translations['SECOND_SCREEN.WASTE-ITEMS.THINGS.ALLOW'].split('; ');
+        this.notAllowThings = translations['SECOND_SCREEN.WASTE-ITEMS.THINGS.NOT-ALLOW'].split('; ');
         this.titleBattery = translations['SECOND_SCREEN.WASTE-ITEMS.BATTERY.TITLE'];
+        this.allowBattery = translations['SECOND_SCREEN.WASTE-ITEMS.BATTERY.ALLOW'].split('; ');
+        this.notAllowBattery = translations['SECOND_SCREEN.WASTE-ITEMS.BATTERY.NOT-ALLOW'].split('; ');
         this.updateDisplay();
       });
   }
@@ -56,116 +98,44 @@ export class WasteService {
       {
         title: this.titlePlastic,
         icon: '../../assets/icons/plastic.svg',
-        allow: [
-          'Plastic: PET, HDPE, LDPE, PE, PP (digital code: 1, 2, 4, 5)',
-          'Boxes (yogurt, salads, kebabs)',
-          'Bottles (oil, ketchup, mayonnaise)',
-          'Canisters from under the water',
-          'Polyethylene bags (bread, milk, cereals)',
-          'Plastic bags',
-          'Packing tape',
-          'Styrofoam',
-          'Packaging for cosmetics (shampoo, washing powder)',
-        ],
-        notAllow: [
-          'Packaging and bags contaminated with food',
-          'Polystyrene packaging (PS, digital code - 6)',
-          'Combined materials (plastic + metal, paper or other types of plastic: C / PAP, C / LDPE, C / ALU, digital code from 80 to 94)',
-          'Disposable tableware',
-          'Stationery',
-          'Tubes for toothpaste and creams (the presence of an aluminum layer inside the tube)',
-          'White jars of sour cream and yogurt',
-          'Candy wrappers',
-          'CD discs',
-          'If the item contains not only plastic (video cassettes, shaving machines), you can separate the materials yourself',
-          'In case of difficulties with the type of plastic - it is better to throw the unknown plastic into the plastic container',
-        ],
+        allow: this.allowPlastic,
+        notAllow: this.notAllowPlastic,
       },
       {
         title: this.titleGlass,
         icon: '../../assets/icons/glass.svg',
-        allow: [
-          'Jars and bottles (alcohol, syrup, oil) ',
-          'All glass containers are recycled',
-        ],
-        notAllow: [
-          'Bulbs ',
-          'Automotive glass ',
-          'Heat-resistant, wired glass ',
-          'Window glass (goes to construction waste) ',
-          'Mirrors ',
-          'Screens of monitors and TVs ',
-          'Colored glass and crystal ',
-          'Faience earthenware',
-        ],
+        allow: this.allowGlass,
+        notAllow: this.notAllowGlass,
       },
       {
         title: this.titleMetal,
         icon: '../../assets/icons/metal.svg',
-        allow: [
-          'Metal lids and plugs ',
-          'Tin cans and boxes (sprats, cat food) ',
-          'All types of metal',
-        ],
-        notAllow: [
-          'Household appliances (there are special collection points for household appliances)',
-        ],
+        allow: this.allowMetal,
+        notAllow: this.notAllowMetal,
       },
       {
         title: this.titleHazardous,
         icon: '../../assets/icons/hazardousWaste.svg',
-        allow: [
-          'Batteries',
-          'Fluorescent lamps',
-          'Mercury-containing thermometers',
-          'Used car oil',
-          'Garden chemicals',
-          'Expired medications',
-          'Accumulator',
-          'Car tires',
-          'Electrical and electronic equipments',
-        ],
-        notAllow: [
-          'All hazardous waste must be disposed of at designated collection points',
-        ],
+        allow: this.allowHazardous,
+        notAllow: this.notAllowHazardous,
       },
       {
         title: this.titleLightBulb,
         icon: '../../assets/icons/lightBulb.svg',
-        allow: [
-          'Fluorescent lamps (contain mercury vapor and inert gases)',
-          'Halogen bulbs (bulbs are very hot and may cause a fire)',
-          'LED lamps (have the best characteristics in terms of energy saving, operation and disposal)',
-          'Incandescent lamps (safe to use)',
-        ],
-        notAllow: ['You can rent any lamps'],
+        allow: this.allowLightBulb,
+        notAllow: this.notAllowLightBulb,
       },
       {
         title: this.titleThings,
         icon: '../../assets/icons/things.svg',
-        allow: [
-          'Items for the poor are handed over to special collection points',
-          'Books',
-          'Furniture for recycling',
-          'Аppliances',
-        ],
-        notAllow: ['Old clothes and shoes'],
+        allow: this.allowThings,
+        notAllow: this.notAllowThings,
       },
       {
         title: this.titleBattery,
         icon: '../../assets/icons/battery.svg',
-        allow: [
-          'Saline',
-          'Alkaline',
-          'Mercury',
-          'Silver',
-          'Lithium',
-          'Nickel-cadmium',
-        ],
-        notAllow: [
-          'Any batteries are recyclable',
-          'Buy rechargeable batteries!',
-        ],
+        allow: this.allowBattery,
+        notAllow: this.notAllowBattery,
       },
     ];
     this.waitChanges$.next();
