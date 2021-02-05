8a166f529e5496fbddf7d596234609f5b20adb4a
Maksim Burakou <maxashtet@gmail.com>
2021-02-05 21:28:37 +0300

    feat: add custom scrolling by the wheel

--- rsclone/src/app/components/content/content.component.ts
+++ rsclone/src/app/components/content/content.component.ts
@@ -1,6 +1,8 @@
 import { IWasteData } from './../../models/wasteData.model';
 import { FirebaseService } from './../../services/firebase.service';
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {EScrollDirection, ScrollService} from '../../services/scroll.service';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-content',
@@ -11,16 +13,40 @@ export class ContentComponent implements OnInit {
   wasteData: IWasteData[];
   userEmail: string;
   menuButton = true;
+  scrollDelay = false;
 
   constructor(
-    public firebaseService: FirebaseService
-  ) {}
+    @Inject(DOCUMENT) private document: Document,
+    public firebaseService: FirebaseService,
+    public scrollService: ScrollService
+  ) {
+    this.document.addEventListener('wheel', this.onContentScrolled.bind(this), { passive: false });
+  }
 
   ngOnInit(): void {
-    this.firebaseService.getData().subscribe((data) => {
-      this.wasteData = this.firebaseService.addDataToObject(data);
-    });
+    // this.firebaseService.getData().subscribe((data) => {
+    //   this.wasteData = this.firebaseService.addDataToObject(data);
+    // });
   }
+
+  onContentScrolled(e): void {
+    e.preventDefault();
+    if (this.scrollDelay) { return; }
+    this.scrollDelay = true;
+    setTimeout(() => {this.scrollDelay = false; }, 500);
+    let delta;
+    if (e.wheelDelta){
+      delta = e.wheelDelta;
+    }else{
+      delta = -1 * e.deltaY;
+    }
+    if (delta < 0){
+      this.scrollService.wheelScroll('up');
+    }else if (delta > 0){
+      this.scrollService.wheelScroll('down');
+    }
+  }
+
   toggleMenuButton(): void{
     this.menuButton = !this.menuButton;
   }
--- rsclone/src/app/services/scroll.service.ts
+++ rsclone/src/app/services/scroll.service.ts
@@ -1,12 +1,46 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+export enum EScrollDirection {
+  Up = 'up',
+  Down = 'down'
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScrollService {
+  activeComponent = 0;
+  selectedComponent = '';
+  componentsList = [
+    'start-screen',
+    'second-screen',
+    // 'map-screen',
+    'info-screen'
+  ];
 
-  constructor() { }
+  constructor() {
+  }
+
   toScroll(idElement: string): void {
-    document.getElementById(idElement).scrollIntoView({ behavior: 'smooth' });
+    this.activeComponent = this.componentsList.indexOf(idElement);
+    document.getElementById(idElement).scrollIntoView({behavior: 'smooth'});
+  }
+
+  wheelScroll(direction): void {
+    if (direction === 'down')
+    {
+      this.activeComponent--;
+    }
+    else
+    {
+      this.activeComponent++;
+      this.activeComponent = this.activeComponent % this.componentsList.length;
+    }
+    if (this.activeComponent < 0)
+    {
+      this.activeComponent = this.componentsList.length - 1;
+    }
+    this.selectedComponent = this.componentsList[this.activeComponent];
+    this.toScroll(this.selectedComponent);
   }
 }
