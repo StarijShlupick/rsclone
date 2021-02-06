d5e09bf1233f2bcb88e256eded9ce6f1f28317fe
Maksim Burakou <maxashtet@gmail.com>
2021-02-06 11:23:25 +0300

    fix: prod build

--- rsclone/src/environments/environment.prod.ts
+++ rsclone/src/environments/environment.prod.ts
@@ -7,5 +7,10 @@ export const environment = {
     storageBucket: "green-map-db.appspot.com",
     messagingSenderId: "910753025457",
     appId: "1:910753025457:web:df0e1634139c3993ea0190"
-  }
+  },
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoicG9sa2hvdnNrYXlhIiwiYSI6ImNrazZ2OWtjOTAzaG4yd280OTY5dG94MGQifQ.3NDcwJ73Jx2PWbB8utg6mw'
+  },
+  mapboxKey: 'pk.eyJ1IjoiYmV6cnVrIiwiYSI6ImNraXJ4bjUxMzA5ZnUydHBkMnVicGFndnMifQ.Pt0kYmMh6o-AeeqT-asOJw',
+  geoapifyKey: '2fa8e80cd0f747d7b45646a4f07fd388'
 };
